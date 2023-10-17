bf610271013287977c673204a3b99e00a818cbf4
zjw57 <zjw57@qq.com>
2023-10-17 17:49:54 +0800

    wireguard

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -2,9 +2,9 @@
 
 # Modify Default IP
 sed -i "s/192.168.1/172.16.1/" package/base-files/files/bin/config_generate
-sed -i "s/192.168.1/172.16.1/" package/feeds/zjw57/base-files/files/bin/config_generate
+# sed -i "s/192.168.1/172.16.1/" package/feeds/zjw57/base-files/files/bin/config_generate
 
 # Remove links
-sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/banner
-sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/openwrt_release
-rm -rf package/feeds/zjw57/my-default-settings/files/usr
+# sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/banner
+# sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/openwrt_release
+# rm -rf package/feeds/zjw57/my-default-settings/files/usr
