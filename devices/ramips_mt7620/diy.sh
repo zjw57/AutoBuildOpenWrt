d53a92b4bc97864eba42b0cbc49e5d95282aa28a
zjw57 <zjw57@qq.com>
2023-10-17 11:59:09 +0800

    default-settings

--- devices/ramips_mt7620/diy.sh
+++ devices/ramips_mt7620/diy.sh
@@ -2,9 +2,9 @@
 
 # Modify Default IP
 sed -i "s/192.168.1/172.16.2/" package/base-files/files/bin/config_generate
-# sed -i "s/192.168.1/172.16.2/" package/feeds/zjw57/base-files/files/bin/config_generate
+sed -i "s/192.168.1/172.16.2/" package/feeds/zjw57/base-files/files/bin/config_generate
 
 # Remove links
-# sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/banner
-# sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/openwrt_release
+sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/banner
+sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/openwrt_release
 rm -f package/feeds/zjw57/my-default-settings/files/usr/lib/lua/luci/view/admin_status/index/links.htm
