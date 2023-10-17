741d51ef649f7d47f776078658fb6a1d47ff7f00
zjw57 <zjw57@qq.com>
2023-10-17 18:54:37 +0800

    patches

--- devices/ramips_mt7620/diy.sh
+++ devices/ramips_mt7620/diy.sh
@@ -8,3 +8,6 @@ sed -i "s/192.168.1/172.16.2/" package/base-files/files/bin/config_generate
 # sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/banner
 # sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/openwrt_release
 # rm -rf package/feeds/zjw57/my-default-settings/files/usr
+
+# uci-defaults
+cp -f devices/common/uci-defaults/* package/base-files/files/etc/uci-defaults/
