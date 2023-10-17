368501370468f49b162572746fd53a3f7cf326b3
zjw57 <zjw57@qq.com>
2023-10-17 14:46:07 +0800

    remove my-default-settings/files/usr

--- devices/ramips_mt7620/diy.sh
+++ devices/ramips_mt7620/diy.sh
@@ -7,4 +7,4 @@ sed -i "s/192.168.1/172.16.2/" package/feeds/zjw57/base-files/files/bin/config_g
 # Remove links
 sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/banner
 sed -i "s/Kiddin'/zjw57/" package/feeds/zjw57/base-files/files/etc/openwrt_release
-rm -f package/feeds/zjw57/my-default-settings/files/usr/lib/lua/luci/view/admin_status/index/links.htm
+rm -rf package/feeds/zjw57/my-default-settings/files/usr
