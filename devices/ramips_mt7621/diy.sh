ebb836a29bd5ece307293e6a0ac48dbdd8dc18e7
zjw57 <zjw57@qq.com>
2023-10-18 01:35:26 +0800

    MT76X easymesh=n

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -11,6 +11,3 @@ sed -i "s/192.168.1/172.16.1/" package/base-files/files/bin/config_generate
 
 # uci-defaults
 cp -f devices/common/uci-defaults/* package/base-files/files/etc/uci-defaults/
-
-# easymesh
-sed -i 's/wpad-wolfssl/wpad-openssl/' package/feeds/zjw57/luci-app-easymesh/Makefile
