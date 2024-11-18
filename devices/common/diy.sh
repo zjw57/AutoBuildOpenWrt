0f95059a9da1ce6949e4e95018d472dcbd947d59
zjw57 <zjw57@qq.com>
2024-11-18 18:36:55 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add Feed Source
-sed -i '$a src-git zjw57 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
+sed -i '$a src-git zjw57 https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
